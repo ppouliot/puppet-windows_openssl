0fe2c6efdc5ca374627cd5f4d5316cbc0fe460b1
Vijay Tripathi <vijayt.124@gmail.com>
2013-12-20 13:35:28 -0800

    Initial Commit

---
+++ manifests/init.pp
@@ -0,0 +1,50 @@
+# === Class: windows_openssl
+#
+# This module installs openssl on Windows systems. It also adds an entry to the
+# PATH environment variable.
+#
+# === Parameters
+#
+# [*url*]
+#   HTTP url where the installer is available. It defaults to main site.
+# [*package*]
+#   Package name in the system.
+# [*file_path*]
+#   This parameter is used to specify a local path for the installer. If it is
+#   set, the remote download from $url is not performed. It defaults to false.
+#
+# === Examples
+#
+# class { 'windows_openssl': }
+#
+# class { 'windows_openssl':
+#   $url     => 'http://192.168.1.1/files/openssl.exe',
+#   $package => 'openssl version 1.8.0-preview201221022',
+# }
+#
+# === Authors
+# 
+#
+class windows_openssl (
+  $url       = $::windows_openssl::params::url,
+  $package   = $::windows_openssl::params::package,
+  $file_path = false,
+) inherits windows_openssl::params {
+
+  if $file_path {
+    $openssl_installer_path = $file_path
+  } else {
+    $openssl_installer_path = "${::temp}\\${package}.exe"
+    windows_common::remote_file{'openssl':
+      source      => $url,
+      destination => $openssl_installer_path,
+      before      => Package[$package],
+    }
+  }
+  package { $package:
+    ensure          => installed,
+    source          => $openssl_installer_path,
+    install_options => ['/VERYSILENT','/SUPPRESSMSGBOXES','/LOG'],
+  }
+  
+}
---
+++ manifests/params.pp
@@ -0,0 +1,4 @@
+class windows_openssl::params {
+  $url     = 'http://slproweb.com/download/Win32OpenSSL-1_0_1e.exe'
+  $package = 'Win32OpenSSL-1_0_1e'
+}
