7f5e563faac55d507adc6844ae0e30a546d670bc
Vijay Tripathi <vijayt.124@gmail.com>
2014-01-07 11:46:50 -0800

    Updating Openssl Package from 1e to 1f

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,4 @@
 class windows_openssl::params {
-  $url     = 'http://slproweb.com/download/Win32OpenSSL-1_0_1e.exe'
+  $url     = 'http://slproweb.com/download/Win32OpenSSL-1_0_1f.exe'
   $package = 'OpenSSL 1.0.1e (32-bit)'
 }
