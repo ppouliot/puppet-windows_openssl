00db67844a1d0f61413894f738bb6c6f027b6fff
gabloe <t-gabloe@microsoft.com>
2014-06-06 09:31:37 -0700

    Fixed broken URL

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,5 @@
 class windows_openssl::params {
-  $url     = 'http://slproweb.com/download/Win32OpenSSL-1_0_1g.exe'
-  $package = 'OpenSSL 1.0.1g (32-bit)'
+  $url          = 'http://slproweb.com/download/Win32OpenSSL-1_0_1h.exe'
+  $package      = 'OpenSSL 1.0.1g (32-bit)'
+  $openssl_path = "C:\\OpenSSL-Win32\\bin"
 }
