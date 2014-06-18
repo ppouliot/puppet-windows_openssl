6738d7958e550dea90cacd55ab5ef6d01c5ed09c
primeministerp <primeministerpete@gmail.com>
2014-06-18 14:12:34 -0400

    updating repo to match production

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,4 @@
 class windows_openssl::params {
-  $url     = 'http://slproweb.com/download/Win32OpenSSL-1_0_1g.exe'
-  $package = 'OpenSSL 1.0.1g (32-bit)'
+  $url     = 'http://slproweb.com/download/Win32OpenSSL-1_0_1h.exe'
+  $package = 'OpenSSL 1.0.1h (32-bit)'
 }
