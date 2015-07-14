24decb35a52d316a0f73871e698eac9ba78b5b37
primeministerp <primeministerpete@gmail.com>
2015-07-14 08:56:40 -0400

    pushing victor's updates

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,4 @@
 class windows_openssl::params {
-  $url     = 'http://slproweb.com/download/Win32OpenSSL-1_0_1i.exe'
-  $package = 'OpenSSL 1.0.1i (32-bit)'
+  $url     = 'http://slproweb.com/download/Win32OpenSSL-1_0_2c.exe'
+  $package = 'OpenSSL 1.0.2c (32-bit)'
 }
